b359dd1463062e233cea9508d2dc3149b2fa3690
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2023-09-22 22:55:13 +0500

    js: bubble sort

---
+++ js/algorithms/bubbleSort.js
@@ -0,0 +1,18 @@
+const arr = [
+  2, 52, 6, 1, 12, 6, 7, 2, 634, 7, 3, 2, 8, 9, 0, 3, 33, 8, 44, 2, 4,
+];
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort(arr));
