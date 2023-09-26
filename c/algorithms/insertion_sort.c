f2029ed8386f215aa14237c1a0a8708a74e7909a
Ahmad Javaid(AJ) <ahmadjavaidwork@gmail.com>
2023-09-26 15:45:17 +0500

    c: insertion sort

---
+++ c/algorithms/insertion_sort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void insertion_sort(int *arr, int size) {
+    int key = 0;
+    int j = 0;
+
+    for (int i; i < size; i++) {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main(void) {
+    int arr_size = 21;
+    int arr[] = {2, 52, 6, 1, 12, 6, 7, 2, 634, 7, 3, 2, 8, 9, 0, 3, 33, 8, 44, 2, 4};
+    insertion_sort(arr, arr_size);
+    for (int i = 0; i < arr_size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
