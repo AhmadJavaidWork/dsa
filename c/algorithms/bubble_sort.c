b5a0994d7105d56a069f01926ccff30f4b0944cc
Ahmad Javaid(AJ) <ahmadjavaidwork@gmail.com>
2023-09-26 15:39:51 +0500

    c: bubble sort

---
+++ c/algorithms/bubble_sort.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void bubble_sort(int *arr, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main(void) {
+    int arr_size = 21;
+    int arr[] = {2, 52, 6, 1, 12, 6, 7, 2, 634, 7, 3, 2, 8, 9, 0, 3, 33, 8, 44, 2, 4};
+    bubble_sort(arr, arr_size);
+    for (int i = 0; i < arr_size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
